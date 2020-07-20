03084c516b1c605767a1a2c4ec61406a4172a579
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-07-20 11:04:48 +0300

    challenge 02 solved

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -27,6 +27,7 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 
 const appendTheEnd = (str) => {
   // Solution code here...
+  return `${str} The end.`
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -44,6 +45,7 @@ console.log(a) prints [1, 2, 3, 1]
 
 const appendFirstToLast = (arr) => {
   // Solution code here...
+//   arr.push(arr[0]);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -63,6 +65,7 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 
 const addBirthYearProperty = (obj, year) => {
   // Solution code here...
+//   obj.yearBorn = year;
 };
 
 /* ------------------------------------------------------------------------------------------------
